28f408722f79e632aa6cefd4af0669a79f526a46
Nathan Snyder <nathan@snydern.com>
2020-05-17 14:37:15 -0400

    Add a bunch of tmux configurations and some theming

--- dotfiles/.scripts/battery.sh
+++ dotfiles/.scripts/battery.sh
@@ -2,4 +2,4 @@
 
 # For inspiration, consider: https://github.com/simonsmith/dotfiles/blob/master/bin/battery
 # IMO, it's way overcomplicated, but there's some good stuff that could be used in there.
-pmset -g batt | grep -Eo "\\d+%"
+pmset -g batt | grep -Eo "(\\d+)%"

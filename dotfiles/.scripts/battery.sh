0404317f5234debd34467e1f06aa2687b6ec2f30
Nathan Snyder <nathan@snydern.com>
2020-05-27 22:11:47 -0400

    Update colors
    Make it clear that the install file is currently Mac-specific.
    Add a brews file
    Add some smart aliases
    Remove the battery.sh file that's no longer used
    Add a file to preview my LS_COLORS

--- dotfiles/.scripts/battery.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# For inspiration, consider: https://github.com/simonsmith/dotfiles/blob/master/bin/battery
-# IMO, it's way overcomplicated, but there's some good stuff that could be used in there.
-pmset -g batt | grep -Eo "(\\d+)%"

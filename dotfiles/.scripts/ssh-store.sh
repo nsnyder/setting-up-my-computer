c7899b1adbda0fc7d4c25ce83f39cd7e764b4519
Nathan Snyder <nathan@snydern.com>
2020-09-04 21:28:08 -0400

    Revert ssh-store change

--- dotfiles/.scripts/ssh-store.sh
+++ dotfiles/.scripts/ssh-store.sh
@@ -36,8 +36,7 @@ fi
 ssh-add -l &>/dev/null
 if [ "$?" = 1 ]; then
     # The agent has no identities. Check if we want to add one.
-    confirm "Would you like to add an SSH identity now? You can always add one later with ssh-store. [Y/n]" &&
-        ssh-add -t 4h
+    confirm "Would you like to add an SSH identity now? You can always add one later with ssh-store. [Y/n]" && ssh-add -t 4h
 fi
 
 

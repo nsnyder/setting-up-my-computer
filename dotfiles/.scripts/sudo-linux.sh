7c5b169fa00b65150b2573c323a0b4a2dd6c9db1
Nathan Snyder <nathan@snydern.com>
2020-09-20 16:03:05 -0400

    Add some linux installers
    Setup Raspbian to only be connected to either wifi or ethernet

---
+++ dotfiles/.scripts/sudo-linux.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Do installation for linux.
+
+apt update && apt upgrade
+apt install network-manager
+
+# Remove these, because they conflict with network-manager.
+apt purge -qq openresolv dhcpcd5
+
+# Copy this file, and make it executable.
+# It must be copied, not symlinked, because NetworkManager requires regular files to work.
+cp $1../network-manager/99-wifi /etc/NetworkManager/dispatcher.d/99-wifi
+chown root:root /etc/NetworkManager/dispatcher.d/99-wifi
+chmod +x /etc/NetworkManager/dispatcher.d/99-wifi
+
+cp $1../network-manager/crda /etc/default/crda
+chown root:root /etc/default/crda
+
+# To handle symlinks correctly, do like so:
+# ln -sf $1../network-manager/99-wifi /etc/NetworkManager/dispatcher.d/99-wifi
